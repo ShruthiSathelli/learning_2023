5dc034d2b1f6b75c46b0f46c23bb2f169fe6e3fa
S.Shruthi <sathellishruthi@gmail.com>
2023-06-22 12:04:48 +0530

    Module1/Day6&7

--- Day6/displaydata.c
+++ Day6/displaydata.c
@@ -1,33 +1,3 @@
-/*#include<stdio.h>
-#include<string.h>
-struct Student{
-    int rollno;
-    char name[20];
-    float perc;
-};
-typedef struct Student Student;
-void DisplayData(Student *student,int size){
-    Student s;
-    for(int i=0;i<size;i++){
-       printf("StudentDetails%d:\n", i + 1);
-       printf("Enter roll no.,Name,percentage:");
-       scanf("%d %c %f",student[i].rollno,student[i].name,student[i].perc);
-    }
-}
-int main(){
-    int size;
-    printf("Enter Number of students:");
-    scanf("%d",&size);
-    Student student[size];
-    for(int i=0;i<size;i++){
-       printf("Student%d:\n", i + 1);
-       printf("Enter roll no.,Name,percentage:");
-       scanf("%d%s%f",&student[i].rollno,student[i].name,&student[i].perc);
-    }
-    DisplayData(student,size);
-    return 0;
-}*/
-
 #include <stdio.h>
 struct Student {
     int rollno;
